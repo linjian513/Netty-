43b45094eab989dac582baa7922b2930d1105793
linjian513 <linjian513@163.com>
2017-08-30 20:24:36 +0800

    Netty权威指南测试例子

---
+++ NettyTest/src/main/java/com/study/NettyTest/Client/TimeClient.java
@@ -0,0 +1,51 @@
+package com.study.NettyTest.Client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+
+	public void connect(String host , int port) throws Exception{
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class)
+				.option(ChannelOption.TCP_NODELAY, true)
+				.handler(new ChannelInitializer<SocketChannel>() {
+
+					@Override
+					protected void initChannel(SocketChannel ch) throws Exception {
+						ch.pipeline().addLast(new TimeClientHandler());
+					}
+				});
+			
+			ChannelFuture f = b.connect(host, port).sync();
+			f.channel().closeFuture().sync();
+			
+		} finally{
+			group.shutdownGracefully();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		int port = 8080;
+		if(args != null && args.length > 0){
+			try {
+				port = Integer.valueOf(args[0]);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+		new TimeClient().connect("127.0.0.1", port);
+		
+	}
+
+}
